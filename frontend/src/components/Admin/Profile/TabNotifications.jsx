5f568fb98007989288b7b038b5106a4770c56c44
sankarjyoti <sankarjyotichetia57@gmail.com>
2026-05-15 19:22:39 +0530

    Fix import path for ToggleButton component

--- frontend/src/components/Admin/Profile/TabNotifications.jsx
+++ frontend/src/components/Admin/Profile/TabNotifications.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ToggleButton from "../ui/ToggleButton";
+import ToggleButton from "../UI/ToggleButton.jsx";
 
 export default function TabNotifications({ prefs, onPrefChange, dark }) {
   return (
